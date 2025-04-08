616b4cd8d87bf002acc0531d3f7d5b3bd00a9ca5
abdemalouki <abdomalouki43@gmail.com>
2025-04-08 10:34:30 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,11 +6,13 @@ app = Flask(__name__)
 @app.route('/ejecutar', methods=['GET'])
 def ejecutar_codigo():
     # Puedes pasar parámetros opcionales desde la URL, ejemplo: ?nombre=juan
-    print(f"Petición recibida")
+    nombre = request.args.get('nombre', 'desconocido')
+    print(f"Petición recibida de: {nombre}")
 
     # Ejecuta tu script
+    subprocess.run(["python3", "mi.py"])
 
-    return  200
+    return f"Código ejecutado por {nombre}", 200
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
