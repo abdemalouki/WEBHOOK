d5c78ae961b9099b30bd53d4563ff3f72f15ee0f
Abdessamad El Mallouki <128838372+abdemalouki@users.noreply.github.com>
2025-04-08 15:05:02 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,4 @@
-from flask import Flask, request
-import subprocess
-
-app = Flask(__name__)
-
-@app.route('/ejecutar', methods=['GET'])
-def ejecutar_codigo():
-    # Puedes pasar parámetros opcionales desde la URL, ejemplo: ?nombre=juan
-    nombre = request.args.get('nombre', 'desconocido')
-    print(f"Petición recibida de: {nombre}")
-
-    # Ejecuta tu script
-    subprocess.run(["python3", "mi.py"])
-
-    return f"Código ejecutado por {nombre}", 200
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+# test_ejemplo.py
+def test_suma():
+    assert 1 + 1 == 2
+test_suma()
